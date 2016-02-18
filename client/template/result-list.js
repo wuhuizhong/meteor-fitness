601cf07838cb8555c8f875ee2c3772871a69ccad
Andrew <andrew.thielcole@gmail.com>
2016-02-17 21:09:29 -0800

    Workout Log

--- client/template/result-list.js
+++ client/template/result-list.js
@@ -1,7 +1,7 @@
 /**
  * Created by Cole on 2/14/16.
  */
-Template.resultList.helpers({
+Template.workoutLog.helpers({
     results: function() {
         return Results.find({} , {sort: {createdAt: -1}});
     }
