e7184c329c2cb3cdc93ef78558add651f3ef9190
Andrew <andrew.thielcole@gmail.com>
2016-02-24 21:47:03 -0800

    Show hide menu

--- client/template/app.js
+++ client/template/app.js
@@ -1,12 +1,39 @@
 /**
  * Created by Cole on 2/14/16.
  */
+Session.setDefault('SHOW_MENU' , true);
 
-    Template.app.helpers({
-        user: function () {
-            return Meteor.user().emails[0].address;
-        }
+Meteor.startup(function () {
+    // set up a swipe left / right handler
+    $(document.body).touchwipe({
+        wipeLeft: function () {
+            Session.set('SHOW_MENU', false);
+        },
+        wipeRight: function () {
+            Session.set('SHOW_MENU', true);
+        },
+        preventDefaultEvents: false
     });
+});
+Template.app.helpers({
+    user: function () {
+        return Meteor.user().emails[0].address;
+    },
+    menuOpen: function() {
+        if (Session.get('SHOW_MENU')) {
+            return 'showMenu';
+        } else {
+            return false;
+        }
+    },
+    menuShowing: function() {
+        if (Session.get('SHOW_MENU')) {
+            return 'menuShowing';
+        } else {
+            return false;
+        }
+    }
+});
 Template.app.events({
     'click #logout': function() {
         Meteor.logout();
