fc6ba7c7657e78831012bae0830d7d04783a43cd
Andrew <andrew.thielcole@gmail.com>
2016-03-05 19:35:35 -0800

    rework with materialize

--- client/template/results/result.js
+++ client/template/results/result.js
@@ -4,5 +4,11 @@
 Template.result.helpers({
     diagnostic: function() {
         return JSON.stringify(this);
+    },
+    dateFormater: function() {
+        /* Format date */
+        let date = new Date(this.createdAt);
+
+        return '' +  date.toLocaleDateString();
     }
 });
