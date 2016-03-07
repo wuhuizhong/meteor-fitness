478fd0559002dec339b59b1615cbde3e676fa040
Andrew <andrew.thielcole@gmail.com>
2016-03-06 20:55:35 -0800

    Android ready

--- mobile-config.js
+++ mobile-config.js
@@ -1,3 +1,15 @@
 /**
  * Created by Cole on 2/15/16.
  */
+App.info({
+    name: 'Fittist',
+    description: 'A fitness logging and tracking tool. Great for crossfit, or strength and conditioning',
+    version: '0.0.1'
+});
+
+App.icons({
+    'android_ldpi': 'resources/icons/kettlebell.png',
+    'android_mdpi': 'resources/icons/kettlebell.png',
+    'android_hdpi': 'resources/icons/kettlebell.png',
+    'android_xhdpi': 'resources/icons/kettlebell.png'
+});
