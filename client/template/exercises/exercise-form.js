01c064046df4acb1d462ebba76b22ca19338eff8
Andrew <andrew.thielcole@gmail.com>
2016-04-27 11:38:06 -0700

    Complexs

--- client/template/exercises/exercise-form.js
+++ client/template/exercises/exercise-form.js
@@ -37,7 +37,7 @@ Template.addexercise.events({
         var reps = document.createElement('input');
         reps.setAttribute('class' , 'complexReps');
         reps.setAttribute('name' , 'complexReps');
-        reps.setAttribute('placeholder' , 'reps');
+        reps.setAttribute('placeholder' , 'Reps');
 
         // See if it is a complex
         if (!Session.get('isComplex')) {
@@ -49,6 +49,7 @@ Template.addexercise.events({
 
         /* Create Movement input box */
         newMovement.setAttribute('type' , 'text');
+        newMovement.setAttribute('name' , 'movements');
         newMovement.setAttribute('class' , 'form-control typeahead movement-name');
         newMovement.setAttribute('autocomplete','off');
         newMovement.setAttribute('placeholder' , 'Movement Name');
@@ -61,6 +62,53 @@ Template.addexercise.events({
 
         /* Add div to complex */
         complex.appendChild(movement);
+    },
+    'click #finish': function(event , target) {
+        event.preventDefault();
+        /* Add Exercise to workout list */
+        console.log(Session.get('exercises'));
+        /* Add Complex if one doesnt already exist */
+        let complex = { movements:[] , properties:{} };
+        let movements = document.getElementsByClassName('movement');
+        console.log(movements);
+
+        /* Iterate through all movements listed listed */
+        for (var i = 0; i < movements.length; i++) {
+            console.log(movements[i]);
+            let reps = 1;
+
+            if (Session.get('isComplex')) {
+                reps = movements[i].firstElementChild.value;
+            }
+
+            console.log('reps' , reps);
+
+            /* Because of typeahead */
+            var name = document.querySelectorAll('[name=movements]')[i].value;
+            console.log(name);
+
+            /* Assume movement already exists */
+            var m = Movements.findOne({name: name});
+
+            /* Need to rethink how to put multiple movements in complex */
+            for (var j = 0; j < reps; j++) {
+                complex.movements.push(m);
+            }
+            console.log('Complex' , complex);
+        }
+
+        /* Add weight to complex */
+        complex.properties.weight = document.querySelector('[name=weight]').value;
+
+        /* Add complex to complex list */
+        // Meteor.call('addComplex' , complex);
+
+        /* Add Complex to exercise list */
+        let exercises = Session.get('exercises');
+        exercises.push(complex);
+        console.log(exercises);
+        Session.set('exercises' , exercises);
+        console.log('setexercises');
     }
 });
 
